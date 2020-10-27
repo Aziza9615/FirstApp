a6e30426bd71fc2598f764b5825d23e72b486188
Aziza9615 <azizisadykova@gmail.com>
2020-10-27 16:00:20 +0600

    lesson 006

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -1,42 +1,75 @@
 package com.example.firstapp
 
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.constrant_layout.*
 
 class MainActivity : AppCompatActivity() {
 
-    val imageArray: MutableList<Int> = mutableListOf<Int>()
+    var count = 0
+    var age = 0
+    var weight = 0
+    var size = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.constrant_layout)
-        changeIconAction()
-
+        Log.v("ACTIVITY_STATE", "onCreate")
     }
 
-    private fun changeIconAction() {
-        change_image.setOnClickListener {
-            val imageArray =
-                arrayOf(R.mipmap.djakarta,R.mipmap.kaliforniya,R.mipmap.chehiya, R.mipmap.horvatiya, R.mipmap.italy, R.mipmap.spain, R.mipmap.peterburg, R.mipmap.tokyo, R.mipmap.turciya)
-
-            val imageRandom = imageArray.random()
-            image_view.setImageResource(imageRandom)
-        }
+    override fun onStart() {
+        super.onStart()
+        Log.v("ACTIVITY_STATE", "onStart")
     }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        count = savedInstanceState.getInt("count")
+        age = savedInstanceState.getInt("age")
+        weight = savedInstanceState.getInt("weight")
+        size = savedInstanceState.getInt("size")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.v("ACTIVITY_STATE", "onResume")
+        count++
+        Log.v("ACTIVITY_STATE", "$count")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.v("ACTIVITY_STATE", "onPause")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.v("ACTIVITY_STATE", "onSaveInstanceState")
+        outState.putInt("count", count)
+        outState.putInt("age", age)
+        outState.putInt("weight", weight)
+        outState.putInt("size", size)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.v("ACTIVITY_STATE", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.v("ACTIVITY_STATE", "onDestroy")
+    }
+
+
+    //private fun showToast(message: String) {
+        //Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+    //}
+
 }
 
-//            val number: Int = (0..1000).random()
-//            showToast("Рандомное число равно = $number")
-//        }
-//
-//    }
-//
-//    private fun showToast(message: String) {
-//        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
-//    }
-//
-//}
 
 
