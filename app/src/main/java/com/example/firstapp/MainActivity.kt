f815eefa54e474713ed0e3c3787d0a2b189c27fc
Aziza9615 <azizisadykova@gmail.com>
2020-11-11 22:06:06 +0600

    test))

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -5,25 +5,42 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var textArray = mutableListOf<String>()
 
-    private lateinit var shared: SharedPreferences
+    //private lateinit var shared: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        shared = SharedPreferences(this)
-
-        shared.logout = false
-        logoutAction()
+        //shared = SharedPreferences(this)
+        addArray()
+        removeArray()
     }
 
-    private fun logoutAction() {
-        logout_button.setOnClickListener click@{
-            shared.login = ""
-            shared.confirmPassword = ""
-            shared.logout = true
+    private fun addArray() {
+        add_button.setOnClickListener click@{
+            val value: String = add_edit_text.text.toString()
+                add_edit_text.text.clear()
+            showToast(this, resources.getString(R.string.Item_not_found))
+                textArray.add(value)
+                displayArray()
+            }
+        }
 
-            intentToNext(this, LoginActivity::class.java)
+    private fun removeArray() {
+        remove_button.setOnClickListener {
+            val value: String = remove_edit_text.text.toString()
+                remove_edit_text.text.clear()
+                showToast(this, resources.getString(R.string.Cant_add_empty_value))
+                textArray.remove(value)
+                displayArray()
+            }
+        }
+
+    private fun displayArray() {
+        var result = ""
+        for (text: String in textArray) {
+            result = "$result + $text \n"
         }
     }
 }
