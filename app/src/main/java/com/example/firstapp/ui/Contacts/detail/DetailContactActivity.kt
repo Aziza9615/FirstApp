64ebdb0fa8860def17dfa42c945fa095695b7a2e
Aziza9615 <azizisadykova@gmail.com>
2020-12-03 23:40:33 +0600

    lesson 23 home work

--- app/src/main/java/com/example/firstapp/ui/Contacts/detail/DetailContactActivity.kt
+++ app/src/main/java/com/example/firstapp/ui/Contacts/detail/DetailContactActivity.kt
@@ -3,10 +3,19 @@ package com.example.firstapp.ui.Contacts.detail
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.firstapp.R
+import android.support.v7.widget.Toolbar
+import android.widget.Toolbar
+import kotlinx.android.synthetic.main.activity_detail_contact.*
 
-class DetailContactActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_contact)
+public class DetailContactActivity : AppCompatActivity() {
+    private Toolbar toolbar:
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_contact);
+        toolbar = findViewById(R.id.toolbar);
+
+
+        setSupportActionBar(toolbar)
     }
 }
