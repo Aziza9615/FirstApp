ae46a95315ead257e23daac21a31343c55d247d2
Aziza9615 <azizisadykova@gmail.com>
2021-01-21 13:11:06 +0600

    35 home work

--- app/src/main/java/com/example/firstapp/ui/Contacts/helper/adapter/ProfileFragment.kt
+++ app/src/main/java/com/example/firstapp/ui/Contacts/helper/adapter/ProfileFragment.kt
@@ -14,6 +14,6 @@ class ProfileFragment() : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_image, container, false)
+        return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 }
